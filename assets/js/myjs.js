795add620ec6517c85ccbdd3f2af9c6c7de6cbe8
Adriel <aponte96.la@gmail.com>
2018-07-19 20:08:52 -0400

    Tercer Punto completado

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,5 +1,10 @@
 $(function () {
     $(".card").on("click", function () {
         $(this).toggleClass("card--open");
-    })
+    });
+    $(".card__like").on("click", function (e) {
+        $(this).toggleClass("card__like--red");
+        e.stopPropagation();
+    });
+
 })
